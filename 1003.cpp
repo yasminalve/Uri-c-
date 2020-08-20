a0edc91acd0fc7a5f2c3dbc0868a4d76fdd2f184
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 19:47:37 -0300

    Create 1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,16 @@
+//Yasmin Alves
+//26.03.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int A, B, SOMA;
+
+    cin >> A >> B;
+    SOMA = A + B;
+    cout << "SOMA = " << SOMA << endl;
+ 
+    return 0;
+}
