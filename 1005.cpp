36b0233e2d943d6de46606f9fc9f27d5a959019b
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 19:51:40 -0300

    Create 1005.cpp

---
+++ 1005.cpp
@@ -0,0 +1,20 @@
+//Yasmin Alves
+//26.03.2020
+
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    double A, B, MEDIA;
+
+    cin >> A >> B;
+    MEDIA = ((3.5*A)+(7.5*B))/11;
+
+    cout << fixed << setprecision(5);
+    cout << "MEDIA = " << MEDIA << endl;
+ 
+    return 0;
+}
