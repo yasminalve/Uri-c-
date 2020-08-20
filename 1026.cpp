39fcb8db28e4523c74ca6010e0754e1059b66547
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 20:19:54 -0300

    Create 1026.cpp

---
+++ 1026.cpp
@@ -0,0 +1,16 @@
+//Yasmin Alves
+//18.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    unsigned long int A, B;
+    
+    while (cin >> A >> B) {
+        cout << (A ^ B) << endl;
+    }
+ 
+    return 0;
+}
