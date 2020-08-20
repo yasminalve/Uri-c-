1552b4f2d7dba11f47f52a1fa396f0e080b0e81d
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 20:12:38 -0300

    Create 1016.cpp

---
+++ 1016.cpp
@@ -0,0 +1,15 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int N;
+    cin >> N;
+    
+    cout << 2*N << " minutos" << endl;
+ 
+    return 0;
+}
