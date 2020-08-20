11a1066cfcb4324e6283eab7eb45cf51d3dc1523
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 19:45:59 -0300

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,16 @@
+//Yasmin Alves
+//26.03.2001
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int A, B, X;
+
+    cin >> A >> B;
+    X = A + B;
+    cout << "X = " << X << endl;
+ 
+    return 0;
+}
