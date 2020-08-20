17c5bed1be8102bbc99a07d4b05eab9af3f385ef
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 19:44:41 -0300

    Create 1007.cpp

---
+++ 1007.cpp
@@ -0,0 +1,17 @@
+//Yasmin Alves
+//26.03.2020
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main () {
+    double A, B, C, D, DIFERENCA;
+
+    cin >> A >> B >> C >> D;
+    DIFERENCA = (A * B - C * D);
+
+    cout << "DIFERENCA = " << DIFERENCA << endl;
+
+    return 0;
+}
