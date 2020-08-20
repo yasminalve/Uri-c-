a1001bcef854fecfb9245b233df54f412e504c6b
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 19:46:49 -0300

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,19 @@
+//Yasmin Alves
+//26.03.2020
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    double raio, area, pi = 3.14159;
+
+    cin >> raio;
+    area = pi*raio*raio;
+
+    cout << fixed << setprecision (4);
+    cout << "A=" << area << endl;
+ 
+    return 0;
+}
