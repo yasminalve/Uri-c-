a67f896268da88b4b3d83fef5ad5017ac2f9c786
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 21:09:33 -0300

    Create 1075.cpp

---
+++ 1075.cpp
@@ -0,0 +1,16 @@
+//Yasmin Alves
+//15.07.2020
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+     int n;
+    cin >> n;
+    for (int i = 1; i < 10000; i ++) {
+        if (i % n == 2) cout << i << endl;
+    }
+ 
+    return 0;
+}
