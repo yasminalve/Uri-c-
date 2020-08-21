f0b2b7e22e2ef8aff6110989330ad82d56c3d35c
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-21 20:50:23 -0300

    Create 1186.cpp

---
+++ 1186.cpp
@@ -0,0 +1,42 @@
+//Yasmin Alves
+//03.07.2020
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+
+float soma ( float M[12][12]);
+ 
+int main() {
+ 
+    char T;
+    float temp, M[12][12];
+
+    cin >> T;
+
+    for (int i = 0; i < 12; i ++) {
+        for (int j = 0; j < 12; j++) {
+            cin >> temp;
+            M[i][j] = temp;
+        }
+    }
+    cout << fixed << setprecision(1);
+    if (T == 'S') cout << soma(M) << endl;
+    else if (T == 'M') cout << soma(M)/66 << endl;
+
+
+
+
+ 
+    return 0;
+}
+float soma (float M[12][12]) {
+    float som = 0;
+    for (int i = 0; i < 12; i ++) {
+        for (int j = 0; j < 12; j++) {
+            if (j > 11 - i) som += M[i][j];
+            else som += 0;
+        }
+    }
+    return som;
+}
