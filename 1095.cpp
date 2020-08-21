afd43398115d156bcf2f1ecdafd77abb3e031560
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-20 21:14:18 -0300

    Create 1095.cpp

---
+++ 1095.cpp
@@ -0,0 +1,15 @@
+//Yasmin Alves
+//11.07.2020
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    for (int i = 1, j = 60; j >= 0; i += 3, j -= 5) {
+        cout << "I=" << i << " J=" << j << endl;
+    }
+ 
+    return 0;
+}
