ffdce472be3a9a5a98a4c6cefd7576e16d9a0843
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-21 20:23:09 -0300

    Create 1143.cpp

---
+++ 1143.cpp
@@ -0,0 +1,16 @@
+//Yasmin Alves
+//27.07.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << i << " " << i*i << " " << i*i*i << endl;
+    }
+ 
+    return 0;
+}
