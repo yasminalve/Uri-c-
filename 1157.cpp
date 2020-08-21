cd78a786e5320bff910201266b50ae0bad5a6312
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-21 20:31:13 -0300

    Create 1157.cpp

---
+++ 1157.cpp
@@ -0,0 +1,19 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int N;
+    cin >> N;
+    
+    for (int i = 1; i <= N; i ++) {
+        if (N % i == 0) {
+            cout << i << endl;
+        }
+    }
+ 
+    return 0;
+}
