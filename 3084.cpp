c3f18205239a4024b555e62d783bef10c64b6f1c
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 22:10:53 -0300

    Create 3084.cpp

---
+++ 3084.cpp
@@ -0,0 +1,16 @@
+//Yasmin Alves
+//14.07.2020
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int x, y;
+    while (cin >> x >> y) { 
+        printf("%.2d:%.2d\n", x/30, y/6);
+    }
+   
+    return 0;
+}
