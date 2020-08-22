ba0c60ca8c56f8d065f677ccef21ad2e63793940
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:03:21 -0300

    Create 1924.cpp

---
+++ 1924.cpp
@@ -0,0 +1,19 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+    string curso;
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> curso;
+    }
+    cout << "Ciencia da Computacao" << endl;
+ 
+    return 0;
+}
