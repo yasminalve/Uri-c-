8459a84a6ca9a468de5968e1dc2d8ad91d280c81
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:18:26 -0300

    Create 2339.cpp

---
+++ 2339.cpp
@@ -0,0 +1,17 @@
+//Yasmin Alves
+//11.07.2020
+#include <iostream>
+#include <stdio.h> 
+ 
+using namespace std;
+ 
+int main() {
+
+    int i, j, k, cont = 1;
+    cin >> i >> j >> k;
+
+    if (j >= i*k) cout << "S" << endl;
+    else cout << "N" << endl;
+ 
+    return 0;
+}
