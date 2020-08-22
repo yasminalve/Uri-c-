c37cc8b452f9424d3f8a21cfefa95e24561e4d06
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:45:45 -0300

    Create 2679.cpp

---
+++ 2679.cpp
@@ -0,0 +1,15 @@
+//Yasmin Alves
+//11.07.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int i;
+    cin >> i;
+    if (i % 2 == 0) cout << i + 2 << endl;
+    else cout << i + 1 << endl;
+ 
+    return 0;
+}
