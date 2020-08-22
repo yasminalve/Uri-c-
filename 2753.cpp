13f9ec28de8a07848bc97d7e7a1e85d1e6c25cff
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:55:37 -0300

    Create 2753.cpp

---
+++ 2753.cpp
@@ -0,0 +1,18 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n; char b;
+    
+    for (int i = 97; i <= 122; i ++) {
+        n = i;
+        b = i;
+        cout << n << " e " << b << endl;
+    }
+ 
+    return 0;
+}
