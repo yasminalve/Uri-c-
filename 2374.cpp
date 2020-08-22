9c057e6bf76b7f9afd14811a3c2a8fee47361e65
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:22:40 -0300

    Create 2374.cpp

---
+++ 2374.cpp
@@ -0,0 +1,17 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int N, M;
+    cin >> N >> M;
+    cout << fixed << setprecision (1);
+    cout << N - M << endl;
+
+ 
+    return 0;
+}
