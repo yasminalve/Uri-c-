fef7e46e0e37cf27b1bef69ad006c9aaec5318a6
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:41:57 -0300

    Create 2483.cpp

---
+++ 2483.cpp
@@ -0,0 +1,18 @@
+//Yasmin Alves
+//11.07.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int i;
+    cin >> i;
+    cout << "Feliz nat";
+    while (i--) {
+        cout << "a";
+    }
+    cout << "l!" << endl;
+ 
+    return 0;
+}
