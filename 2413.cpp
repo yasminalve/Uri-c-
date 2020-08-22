dad184681699ed9af581a597bd6e8a3b7bd40e11
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:24:31 -0300

    Create 2413.cpp

---
+++ 2413.cpp
@@ -0,0 +1,17 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int P, S, T;
+    
+    cin >> T;
+    
+    S = 2*T;
+    P = 2*S;
+    
+    cout << P << endl;
+    return 0;
+}
