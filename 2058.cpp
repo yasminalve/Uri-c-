b0f6a67646976d3f9f5f0bbbc653d357091c4253
yasminalve <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:08:42 -0300

    Create 2058.cpp

---
+++ 2058.cpp
@@ -0,0 +1,16 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int N;
+    
+    cin >> N;
+    
+    cout << N - 2 << endl;
+ 
+    return 0;
+}
