449bdb12e32e40a2ae6b4121a1561b3bdd2be23f
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 21:44:49 -0300

    Create 2581.cpp

---
+++ 2581.cpp
@@ -0,0 +1,22 @@
+//Yasmin Alves
+//11.07.2020
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+ 
+using namespace std;
+ 
+int main() {
+
+    int n; string s;
+
+    cin >> n;
+    
+    for (int i = 0; i < n; i ++) {
+        cin >> s;
+        cout << "I am Toorg!" << endl; 
+    }
+ 
+    return 0;
+}
