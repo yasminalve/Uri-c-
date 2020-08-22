36808c92bb5614c4a385a0a821fb1d6158ce2a2a
Yasmin Alves <69992737+yasminalve@users.noreply.github.com>
2020-08-21 22:03:50 -0300

    Create 2861.cpp

---
+++ 2861.cpp
@@ -0,0 +1,20 @@
+//Yasmin Alves
+//10.06.2020
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int C;
+    cin >> C;
+    
+    for (int i = 0; i < C; i++) {
+        string pergunta;
+        cin >> pergunta;
+        cout << "gzuz" << endl;
+        
+    }
+ 
+    return 0;
+}
